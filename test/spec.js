2c68b62c8cfc6e4a734e9b16e49b26c4dd68a81a
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-03-20 11:08:39 +0100

    Working test

--- test/spec.js
+++ test/spec.js
@@ -1,9 +1,9 @@
 var fs        = require('fs')
+var path      = require('path')
 var assert    = require('assert')
 var app_pony  = require('./app')
 
 it('will base64 read images and add required prefix', function() {
-    var read_pony = fs.readFileSync('./pony', 'base64')
-    console.log(read_pony)
-    assert(false)
+    var read_pony = fs.readFileSync(path.resolve(__dirname, './pony.jpg'), 'base64')
+    assert(app_pony == 'data:image/jpg;base64,'+read_pony)
 })
