506453c8f015831cbc9551e43e5c1de421aa019a
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-03-20 11:08:34 +0100

    Removing . from ext for uri

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var orig = require.extensions['.js']
 var loader = function(ext, module, filename) {
     if (filename.indexOf('node_modules') >= 0) return orig(module, filename)
     var _base64 = fs.readFileSync(filename, 'base64')
-    return module._compile('module.exports = "data:image/'+ext+';base64,'+_base64+'"', filename)
+    return module._compile('module.exports = "data:image/'+ext.slice(1)+';base64,'+_base64+'"', filename)
 }
 
 exts.forEach(function(ext) {
