791a4ae16ef88a6f1b192475aa1f13bbd8ff54b4
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-04-12 13:19:58 +0200

    Working on svg support

--- test/spec.js
+++ test/spec.js
@@ -1,9 +1,15 @@
 var fs        = require('fs')
 var path      = require('path')
 var assert    = require('assert')
-var app_pony  = require('./app')
+var pony      = require('./pony.jpg')
+var thumb     = require('./thumb.svg')
 
 it('will base64 read images and add required prefix', function() {
     var read_pony = fs.readFileSync(path.resolve(__dirname, './pony.jpg'), 'base64')
-    assert(app_pony == 'data:image/jpg;base64,'+read_pony)
+    assert(pony == 'data:image/jpg;base64,'+read_pony)
+})
+
+it('supports svg and will inline directly (no base64) + optimize', function() {
+    var read_thumb = fs.readFileSync(path.resolve(__dirname, './thumb.min.svg'), 'utf-8')
+    assert(thumb == 'data:image/svg+xml;utf8,'+read_thumb)
 })
